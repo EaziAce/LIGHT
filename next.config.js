2570760f5911e060e0f9b2743f9ade0f4699794d
EaziAce <Info@kickfixx.com>
2025-10-08 11:40:48 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,1 +1,3 @@
 module.exports = { reactStrictMode: true };
+<Image src="/kickfixx-logo-00B8FF.png" alt="Kickfixx" width={160} height={40} priority />
+<Image src="/vending-mockup.png" alt="Kickfixx vending mockup" width={800} height={520} priority />
